ea9bd030e75e0391c7c9f7c3947fad8a18425b8d
anne <anne.duan@aax.com>
2022-05-10 07:54:23 +0800

    添加image图片

--- javascript/08-虚拟列表/index.js
+++ javascript/08-虚拟列表/index.js
@@ -1,7 +1,8 @@
-const { createApp } = Vue;
-createApp({
-  data() {
-    return {
+const { createApp, reactive, toRefs, computed, onMounted, ref } = Vue;
+const vm = createApp({
+  setup() {
+      const listWrap = ref(null);
+      const viewData = reactive({
         list: [],
         total: 1000, // 数据总条数
         height: 600, // 可视区域的高度
@@ -9,60 +10,67 @@ createApp({
         startIndex: 0, // 初始位置
         endIndex: 0, // 结束位置
         timer: false,
-        bufferSize: 5 // 做一个缓存
-
-    }
-  },
-  computed: {
-    contentStyle() {
-      const {total, rowHeight } = this;
-      return {
-        height: `${total * rowHeight}px`,
-        position: 'relative'
-      }
-    }
-  },
-  methods: {
-    renderData() {
-    this.list = [];
-    const { rowHeight, height, startIndex, total, bufferSize } = this;
-    // 当前可视区域的row条数
-    const limit = Math.ceil(height/rowHeight);
-    console.log(limit, '=limit');
-    // 可视区域的最后一个位置
-    this.endIndex = Math.min(startIndex + limit + bufferSize, total -1);
-      for (let i=startIndex; i<this.endIndex; i++) {
-      this.list.push({
-        content: i,
-        style: {
-          top: `${i * rowHeight}px`
+        bufferSize: 5 // 做一个缓冲
+        
+      });
+      const contentStyle = computed(() => {
+        return {
+          height: `${viewData.total * viewData.rowHeight}px`,
+          position: 'relative',
         }
-      })
-    }
-    }
-  },
-  mounted() {
-    this.renderData();
-    const listWrap = this.$refs['list-wrap'];
-    listWrap && listWrap.addEventListener('scroll', (e) => {
-      if (this.timer) {
-        return;
+      });
+      // todo 设置数据
+      const renderData = () => {
+        viewData.list = [];
+        const {rowHeight, height, startIndex, total, bufferSize} = viewData;
+        // 当前可视区域的row条数
+        const limit = Math.ceil(height/rowHeight);
+        console.log(limit, '=limit');
+        // 可视区域的最后一个位置
+        viewData.endIndex = Math.min(startIndex + limit + bufferSize, total -1);
+          for (let i=startIndex; i<viewData.endIndex; i++) {
+            viewData.list.push({
+            content: i,
+            style: {
+              top: `${i * rowHeight}px`
+            }
+          })
+        }
       }
-      const { rowHeight, startIndex, bufferSize } = this;
-      const { scrollTop } = e.target;
-      // 计算当前滚动的位置，获取当前开始的起始位置
-      const currentIndex = Math.floor(scrollTop / rowHeight); 
-      this.timer = true;
-      // console.log(startIndex, currentIndex);
-      // 做一个简单的节流处理
-      setTimeout(() => {
-           this.timer = false;
-           // 如果滑动的位置不是当前位置
-           if (currentIndex !== startIndex) {
-             this.startIndex = Math.max(currentIndex - bufferSize, 0);
-             this.renderData();
-           }
-        }, 500)
-    })
-  }
-}).mount('#app')
+      // todo 监听滚动，设置statIndex与endIndex
+      const handleScroll = (callback) => {
+        // console.log(listWrap.value)
+        listWrap.value && listWrap.value.addEventListener('scroll', (e) => {
+          if (this.timer) {
+            return;
+          }
+          const { rowHeight, startIndex, bufferSize } = viewData;
+          const { scrollTop } = e.target;
+          // 计算当前滚动的位置，获取当前开始的起始位置
+          const currentIndex = Math.floor(scrollTop / rowHeight); 
+          viewData.timer = true;
+          // console.log(startIndex, currentIndex);
+          // 做一个简单的节流处理
+          setTimeout(() => {
+            viewData.timer = false;
+               // 如果滑动的位置不是当前位置
+               if (currentIndex !== startIndex) {
+                viewData.startIndex = Math.max(currentIndex - bufferSize, 0);
+                callback();
+               }
+            }, 500)
+        })
+      }
+      onMounted(() => {
+        renderData();
+        handleScroll(renderData);
+      })
+      return {
+        ...toRefs(viewData),
+        contentStyle,
+        renderData,
+        listWrap
+      }
+  },
+})
+vm.mount('#app')
