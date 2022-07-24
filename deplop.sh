76ea8792d4dcafdeea0bbafd7d142c83b1fb3c1b
maicFir <anne.duan@aax.com>
2022-07-24 19:58:58 +0800

    $msg

--- deplop.sh
+++ deplop.sh
@@ -8,7 +8,8 @@ git status
 git add .
 
 # 提交
-git commit -m 'update package.json'
+read -p "input commit message: " msg
+git commit -m '$msg'
 # 添加远程remote 如果项目已经remote，可以省略
 # git remote add origin https://github.com/maicFir/lessonNote.git
 # 推送到指定分支
