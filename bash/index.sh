3a08852ee16e16e7a8b0d3c5dd9a7a6e670aef49
maicFir <anne.duan@aax.com>
2022-07-24 10:43:47 +0800

    add xx

---
+++ bash/index.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+echo 'hello bash'
+# 删除test.txt
+# rm test.txt
+# 打开文件，进入终端
+# vim test2.txt
+
+# 删除整个目录
+# rm -rf test
+
+# 查看目录
+ls -a
+
+# copy
+cp test2.txt text2_blank.txt
+
---
+++ deplop.sh
@@ -0,0 +1,17 @@
+# 如果项目已经初始化后，已经init 那么不用
+# git init
+# 更新master
+git pull origin master
+# 查看本地状态
+git status
+# 添加本地修改的文件
+git add .
+
+# 提交
+git commit -m 'add xx'
+# 添加远程remote 如果项目已经remote，可以省略
+# git remote add origin https://github.com/maicFir/lessonNote.git
+# 推送到指定分支
+git push origin master
+
+
